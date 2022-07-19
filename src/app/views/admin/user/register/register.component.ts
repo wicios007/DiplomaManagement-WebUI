eedfec6d059d5cfe4107ca15e57532d74174b237
wicios007 <35377869+wicios007@users.noreply.github.com>
2022-07-19 12:44:22 +0200

    Update register.components.ts

--- src/app/views/admin/user/register/register.component.ts
+++ src/app/views/admin/user/register/register.component.ts
@@ -92,39 +92,20 @@ export class RegisterComponent implements OnInit {
   }
 
   extractErrorMessagesFromErrorResponse  = (errorRes : HttpErrorResponse) => {
-    // 1 - Create empty array to store errors
     const errors = [];
-
-    // 2 - check if the error object is present in the response
     if (errorRes.error) {
-
-      // 3 - Push the main error message to the array of errors
       errors.push(errorRes.error.message);
-
-      // 4 - Check for Laravel form validation error messages object
       if (errorRes.error.errors) {
-
-        // 5 - For each error property (which is a form field)
         for (const property in errorRes.error.errors) {
-
           if (errorRes.error.errors.hasOwnProperty(property)) {
-
-            // 6 - Extract it's array of errors
             const propertyErrors: Array<string> = errorRes.error.errors[property];
-
-            // 7 - Push all errors in the array to the errors array
             propertyErrors.forEach(error => errors.push(error));
           }
-
         }
-
       }
-
     }
-
     return errors;
     }
-
 }
 
 
